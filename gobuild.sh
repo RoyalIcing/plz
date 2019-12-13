e262dc5686b5caae8eb3b8af143b63b057f9d0e3
Patrick Smith <patrick@burntcaramel.com>
2019-12-13 22:02:19 +1100

    Fix build script

--- gobuild.sh
+++ gobuild.sh
@@ -6,12 +6,16 @@ export GOPATH="$(pwd)/.gobuild"
 SRCDIR="${GOPATH}/src/github.com/RoyalIcing/plz"
 
 [ -d ${GOPATH} ] && rm -rf ${GOPATH}
-mkdir -p ${GOPATH}/{src,pkg,bin}
-mkdir -p ${SRCDIR}
-cp tf.go ${SRCDIR}
+mkdir -p "${GOPATH}/src"
+mkdir -p "${GOPATH}/pkg"
+mkdir -p "${GOPATH}/bin"
+mkdir -p "${SRCDIR}"
+cp *.go "${SRCDIR}"
+cp go.mod go.sum "${SRCDIR}"
+cp -R cmd/ "${SRCDIR}/cmd"
 (
-    echo ${GOPATH}
-    cd ${SRCDIR}
+    echo "${GOPATH}"
+    cd "${SRCDIR}"
     go get .
     go install .
 )
